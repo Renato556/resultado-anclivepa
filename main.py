ad523870ca5e84d267fe123cd9fcb07286d016a4
Renato <renatocabral2000@gmail.com>
2025-09-07 12:06:46 -0300

    init + github actions

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+url_entrevistas = 'https://portal.anclivepa-sp.org.br/wp-content/uploads/2025/09/APRIMORAMENTO-HORARIOS-ENTREVISTAS-2025-EXTRAORDINARIO.pdf'
+
+def send_ntfy_notification():
+    try:
+        requests.post(
+            "https://ntfy.sh/anclivepa",
+            data='Clique aqui para acessar o site da anclivepa',
+            headers={
+                "Title": "Site mudou!!!!!!",
+                "Priority": "high",
+                "Tags": "warning",
+                "Click": "https://portal.anclivepa-sp.org.br/"
+            }
+        )
+        print("-> Notificação via ntfy.sh enviada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar notificação ntfy.sh: {e}")
+
+
+def check_anclivepa():
+    response = requests.get('https://portal.anclivepa-sp.org.br/')
+
+    if url_entrevistas in response.text:
+        print('Resultado da primeira etapa ainda presente')
+        send_ntfy_notification() # Remover
+    else:
+        send_ntfy_notification()
+
+check_anclivepa()
