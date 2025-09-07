159baf49b357964d40ebdec287f175fa76c23667
Renato <renatocabral2000@gmail.com>
2025-09-07 14:19:25 -0300

    v2

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+from requests import RequestException
 
 url_entrevistas = 'https://portal.anclivepa-sp.org.br/wp-content/uploads/2025/09/APRIMORAMENTO-HORARIOS-ENTREVISTAS-2025-EXTRAORDINARIO.pdf'
 
-def send_ntfy_notification():
+def send_ntfy_notification(message):
     try:
         requests.post(
             "https://ntfy.sh/anclivepa",
-            data='Clique aqui para acessar o site da anclivepa',
+            data=message.encode('UTF-8'),
             headers={
                 "Title": "Site mudou!!!!!!",
                 "Priority": "high",
@@ -18,13 +20,53 @@ def send_ntfy_notification():
     except Exception as e:
         print(f"Erro ao enviar notificação ntfy.sh: {e}")
 
+def send_ntfy_notification_error(message):
+    try:
+        requests.post(
+            "https://ntfy.sh/anclivep",
+            data=message.encode('UTF-8'),
+            headers={
+                "Title": "Erro na solicitacao",
+                "Priority": "high",
+                "Tags": "warning",
+            }
+        )
+        print("-> Notificação via ntfy.sh enviada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar notificação ntfy.sh: {e}")
+
 
 def check_anclivepa():
-    response = requests.get('https://portal.anclivepa-sp.org.br/')
+    url = 'https://portal.anclivepa-sp.org.br/'
+    print(f"[{__name__}] Verificando {url}...")
 
-    if url_entrevistas in response.text:
-        print('Resultado da primeira etapa ainda presente')
-    else:
-        send_ntfy_notification()
+    try:
+        response = requests.get(url, timeout=15)
+        response.raise_for_status()
 
-check_anclivepa()
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        slider_items_container = soup.select_one('.jet-slider__items.sp-slides')
+
+        if slider_items_container:
+            slides = slider_items_container.select('.jet-slider__item')
+            current_count = len(slides)
+            print(f"-> Encontrado(s) {current_count} slides no elemento.")
+
+            if current_count != 4:
+                send_ntfy_notification('Quantidade de slides mudou!')
+            elif url_entrevistas not in response.text:
+                send_ntfy_notification('Link de resultados alterado!!!')
+            else:
+                print(f"-> Link permanece igual.")
+        else:
+            print("-> AVISO: Elemento do slider não encontrado na página.")
+            send_ntfy_notification_error('Elemento slide não encontrado na página')
+            return
+
+    except RequestException as e:
+        print(f"Erro ao acessar a URL: {e}")
+        send_ntfy_notification_error('Erro ao acessar URL')
+
+if __name__ == "__main__":
+    check_anclivepa()
